350b61fe8ba563f803321c7dd88de02e654905c0
SumitNalavade <vs.nalavade2003@gmail.com>
2022-01-05 01:20:07 -0600

    setup initial campground model

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Datbase connected");
+});
 
 const app = express();
 
@@ -14,4 +23,4 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
