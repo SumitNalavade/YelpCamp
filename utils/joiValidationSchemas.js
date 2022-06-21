24e4e7a0ca083e347ca2b0315b8c5762b5274f3f
SumitNalavade <vs.nalavade2003@gmail.com>
2022-06-21 18:06:29 -0500

    Remove santitize-html

--- utils/joiValidationSchemas.js
+++ utils/joiValidationSchemas.js
@@ -1,41 +1,19 @@
-const BaseJoi = require('joi');
-const sanitizeHTML = require("sanitize-html")
+const Joi = require("joi");
 
-const extension = (joi) => ({
-    type: 'string',
-    base: joi.string(),
-    messages: {
-        'string.escapeHTML': '{{#label}} must not include HTML!'
-    },
-    rules: {
-        escapeHTML: {
-            validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
-                if (clean !== value) return helpers.error('string.escapeHTML', { value })
-                return clean;
-            }
-        }
-    }
+//Define joi validation schema
+const CampgroundSchema = Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.string().required(),
+    location: Joi.string().required(),
+    description: Joi.string().required(),
+    deleteImage: Joi.array()
 });
 
-const Joi = BaseJoi.extend(extension)
-
-module.exports.campgroundSchema = Joi.object({
-    campground: Joi.object({
-        title: Joi.string().required().escapeHTML(),
-        price: Joi.number().required().min(0),
-        location: Joi.string().required().escapeHTML(),
-        description: Joi.string().required().escapeHTML()
-    }).required(),
-    deleteImages: Joi.array()
+const ReviewSchema = Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    body: Joi.string().required()
 });
 
-module.exports.reviewSchema = Joi.object({
-    review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required().escapeHTML()
-    }).required()
-})
+exports.CampgroundSchema = CampgroundSchema
+exports.ReviewSchema = ReviewSchema;
