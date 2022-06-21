4916a1fba728b0cbae195c4a7377a5c8883ca1bf
SumitNalavade <vs.nalavade2003@gmail.com>
2022-06-21 18:08:08 -0500

    Fix image bug

--- utils/joiValidationSchemas.js
+++ utils/joiValidationSchemas.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const CampgroundSchema = Joi.object({
     title: Joi.string().required(),
     price: Joi.number().required().min(0),
-    image: Joi.string().required(),
+    // image: Joi.string().required(),
     location: Joi.string().required(),
     description: Joi.string().required(),
     deleteImage: Joi.array()
